4dc17cfda33dcd86864012b200db38af9370b4c4
laisiyuan <laisiyuan@meimiao.net>
2017-03-30 17:51:15 +0800

    加入后台

--- lar/routes/web.php
+++ lar/routes/web.php
@@ -15,10 +15,32 @@
 //     return view('welcome');
 //});
 
+// Blog Pages
 Route::get('/', function(){
-     return redirect('/blog');
+     return redirect('login');
 });
 
 Route::get('blog', 'BlogController@index');
 
 Route::get('blog/{slug}', 'BlogController@showPost');
+
+// Admin area
+
+Route::get('admin', function(){
+	return redirect('/admin/post');
+});
+
+Route::group(['namespace' => 'Admin', 'middleware' => 'auth'], function (){
+	Route::resource('admin/post', 'PostController');
+	Route::resource('admin/tag', 'TagController');
+	Route::get('admin/upload', 'UploadController@index');
+});
+
+// Loggin in and out
+Route::get('auth/login', 'Auth\LoginController@login');
+//Route::get('auth/login', 'Auth\LoginController@postLogin');
+Route::get('auth/logout', 'Auth\LoginController@logout');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
