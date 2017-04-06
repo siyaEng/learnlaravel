bce8560c9fd93cbb8c5671738c3e5dec7647276a
laisiyuan <laisiyuan@meimiao.net>
2017-04-06 17:29:11 +0800

    修改文件大小不显示的情况

--- lar/app/help.php
+++ lar/app/help.php
@@ -9,7 +9,7 @@ function human_filesize($bytes, $decimals = 2)
 	$size = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
 	$factor = floor((strlen($bytes) - 1) / 3);
 
-	return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @size[$factor];
+	return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
 }
 
 /**
