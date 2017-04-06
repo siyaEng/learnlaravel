83cede1b91548d5f28d72684fa7e1b13b603734e
laisiyuan <laisiyuan@meimiao.net>
2017-04-06 20:01:15 +0800

    后台文章增删改查

--- lar/app/Post.php
+++ lar/app/Post.php
@@ -8,12 +8,28 @@ class Post extends Model
 {
     protected $dates = ['published_at'];
 
+    /**
+     * Tht many-to-many relationship between posts and tags
+     *
+     * @rerurn BelongsToMany
+     */
+    public function tags()
+    {
+    	return $this->belongsToMany('App\Tag', 'post_tag_pivot');
+    }
+
+    /**
+     * Set the title attribute and automatically the slug
+     * 
+     * @param string $value
+     */
     public function setTitleAttribute($value)
     {
     	$this->attributes['title'] = $value;
 
     	if(! $this->exists) {
-    		$this->attributes['slug'] = str_slug($value);
+    		//$this->attributes['slug'] = str_slug($value);
+    		$this->setUniqueSlug($value, '');
     	}
     }
 }
