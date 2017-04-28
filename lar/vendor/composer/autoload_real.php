6cc5a739bd9be8c445d2df4f4f64d0eadab46118
root <root@iZ8vbc5yk331e10v3iobajZ.(none)>
2017-04-28 16:46:02 +0800

    composer update

--- lar/vendor/composer/autoload_real.php
+++ lar/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit2563e6209af3c478ba1f484d766c8a5c
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit2563e6209af3c478ba1f484d766c8a5c', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
