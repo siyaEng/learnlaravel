afd5beddab92d0f40b8a26c2f024b2684fce3b8b
laisiyuan <laisiyuan@meimiao.net>
2017-05-02 10:24:38 +0800

    增加评论区

--- lar/app/Http/Controllers/BlogController.php
+++ lar/app/Http/Controllers/BlogController.php
@@ -31,6 +31,6 @@ class BlogController extends Controller
             $tag = Tag::whereTag($tag)->firstOrFail();
         }
 
-        return view($post->layout, compact('post', 'tag'));
+        return view($post->layout, compact('post', 'tag', 'slug'));
     }
 }
--- lar/resources/views/blog/partials/page-footer.blade.php
+++ lar/resources/views/blog/partials/page-footer.blade.php
@@ -1,3 +1,11 @@
+@if(isset($slug) && $slug)
+<hr>
+<div class="container">
+  <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+    @include('blog.partials.disqus')
+  </div>
+</div>
+@endif
 <hr>
 <footer>
   <div class="container">
